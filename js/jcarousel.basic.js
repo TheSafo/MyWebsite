ecd21eb507feafe2c3764ca7b46f6279cd2af2f4
Jake Saferstein <jakesafo@gmail.com>
2016-05-04 02:13:37 -0400

    Clos-ish to getting slider to wroking

--- js/jcarousel.basic.js
+++ js/jcarousel.basic.js
@@ -1,6 +1,47 @@
 (function($) {
     $(function() {
-        $('.jcarousel').jcarousel();
+
+    $('.jcarousel')
+        .on('jcarousel:create', function() {
+
+            console.log('Created slider');
+
+            var element = $(this),
+                width = element.innerWidth();
+
+            if(width < 100)
+            {
+                                    // var width = carousel.innerWidth();
+                    width = ($(window).width() * .8 * .8 -20) * .9;
+
+                    // console.log('width '+width);
+                    // carousel.jcarousel('xxx').css('width', width + 'px');
+            }
+
+            console.log('Width: ' + width);
+
+            // This shows 1 item at a time.
+            // Divide `width` to the number of items you want to display,
+            // eg. `width = width / 3` to display 3 items at a time.
+            element.jcarousel('items').css('width', width + 'px');
+        })
+        .on('jcarousel:reload', function() {
+            console.log('Reload slider');
+
+            var element = $(this),
+                width = element.innerWidth();
+
+            console.log('Width: ' + width);
+
+            // This shows 1 item at a time.
+            // Divide `width` to the number of items you want to display,
+            // eg. `width = width / 3` to display 3 items at a time.
+            element.jcarousel('items').css('width', width + 'px');
+        })
+        .jcarousel({
+            // Your configurations options
+        });
+
 
         $('.jcarousel-control-prev')
             .on('jcarouselcontrol:active', function() {
@@ -13,6 +54,7 @@
                 target: '-=1'
             });
 
+
         $('.jcarousel-control-next')
             .on('jcarouselcontrol:active', function() {
                 $(this).removeClass('inactive');
@@ -23,6 +65,7 @@
             .jcarouselControl({
                 target: '+=1'
             });
+
 
         $('.jcarousel-pagination')
             .on('jcarouselpagination:active', 'a', function() {
